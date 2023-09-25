cad6fdf9ffa4e9c1a4a8c617e0d8cb39f5dc8c3d
JiaYe <planet2@qq.com>
2023-09-25 09:23:22 +0800

    保存代码

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod downloader;
+mod config;
+mod def;
+
 fn main() {
     #[cfg(desktop)]
     satellite_wallpaper_android_lib::run();
